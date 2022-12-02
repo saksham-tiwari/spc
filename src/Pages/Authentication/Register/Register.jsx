390ee6053b3543a0d7bdfd0140f4e1539287317a
hargun0360 <hargunsingh0720@gmail.com>
2022-12-03 01:31:20 +0530

    Add Validation in Register Form

--- src/Pages/Authentication/Register/Register.jsx
+++ src/Pages/Authentication/Register/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import login from "../../../Assets/login.svg"
 import "./Register.css"
 import EmailIcon from '@mui/icons-material/Email';
@@ -9,23 +9,46 @@ import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import { signup } from '../../../server/services/auth/auth.service';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import Spinner from '../../../Components/Spinner/Spinner';
 
 const Register = () => {
     const navigate = useNavigate();
-    const submit = (e)=>{
+
+    const [toggle, setToggle] = useState(false);
+    const [toggle2, setToggle2] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const { register, handleSubmit, formState: { errors }, reset, watch } = useForm({
+        mode: "onTouched"
+    });
+
+    const onSubmit = (data, e) => {
         e.preventDefault();
-        const myFormData = new FormData(e.target);
-        const formDataObj = {};
-        myFormData.forEach((value, key) => (formDataObj[key] = value));
+        setLoading(true);
+        let formDataObj = {
+            email : data.email,
+            Name : data.Name,
+            phoneNumber : data.phoneNumber,
+            password : data.password,
+        }
         console.log(formDataObj);
         signup(formDataObj)
-        .then((res)=>{
-            navigate("/otp",{state:{email:formDataObj.email}})
+        .then((res) => {
+            setLoading(false);
+            navigate("/otp", { state: { email: formDataObj.email } })
             console.log(res);
         })
-        .catch((err)=>{console.log(err);})
+        .catch((err) => { console.log(err);
+            setLoading(false); })
+        reset();
     }
+
     return (<>
+        {
+            loading ? <Spinner /> : null
+        }
         <div className='Page-content'>
             <div className='left-side'>
                 <div className='login-image-pos'>
@@ -41,61 +64,86 @@ const Register = () => {
                     </div>
                     <div className='login-para'>
                         <p style={{ textAlign: 'center' }}>
-                            Please enter your details <br/>
+                            Please enter your details <br />
                             to create your account in 1 go.
                         </p>
                     </div>
-                   
-                    <form onSubmit={submit}>
-                        <div className='email mb-4'>
+
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className='email mb-1'>
                             <label className='py-2'>Email</label>
                             <div className='email-icon'>
                                 <EmailIcon />
                             </div>
-                            <input name="email" type="email" placeholder='xyz@akgec.ac.in' required />
+                            <input type="email" placeholder='xyz@akgec.ac.in' name="email" {...register("email", { required: "email is required", pattern: { value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]{4,}[@][a][k][g][e][c][\.][a][c][\.][i][n]$/i, message: "this is not a valid email" } })} />
+                            <p className='alerts'>{errors.email?.message}</p>
                         </div>
-                        <div className='email mb-4'>
+                        <div className='email mb-1'>
                             <label className='py-2'>Name</label>
                             <div className='email-icon'>
                                 <PersonIcon />
                             </div>
-                            <input name="Name" type="text" placeholder='Name' required />
+                            <input name="Name" type="text" placeholder='Name' {...register("Name", { required: "name is required", pattern: { value: /^[a-zA-Z]{1,}$/i, message: "this is not a valid name" } })} />
+                            <p className='alerts'>{errors.Name?.message}</p>
                         </div>
-                        <div className='email mb-4'>
+                        <div className='email mb-1'>
                             <label className='py-2'>Phone Number</label>
                             <div className='email-icon'>
                                 <PhoneIcon />
                             </div>
-                            <input name="phoneNumber" type="number" placeholder='9997788654' required />
+                            <input type="number" placeholder='9956118028' name="phoneNumber" {...register("phoneNumber", { required: "phone number is required", pattern: { value: /^[6789]\d{9}$/i, message: "this is not a valid phone number" } })} />
+                            <p className='alerts'>{errors.phoneNumber?.message}</p>
                         </div>
-                        <div className='password mb-4'>
+                        <div className='password mb-1'>
                             <label className='py-2'>New Password</label>
                             <div className='password-icon'>
                                 <LockIcon />
                             </div>
-                            <div className='eye-icon'>
-                                <VisibilityOffIcon />
-                            </div>
-                            <input name="password" type="password" placeholder='6+ Characters' required/>
-                     
+
+                            {
+                                toggle ? <div className='eye-icon' onClick={() => { setToggle(!toggle) }}>
+                                    <VisibilityIcon />
+                                </div> : <div className='eye-icon' onClick={() => { setToggle(!toggle) }}>
+                                    <VisibilityOffIcon />
+                                </div> 
+                            }
+
+                            <input type={toggle ? "text" : "password"} placeholder='7+ Characters' name="password" {...register("password", { required: "password is required", minLength: { value: 8, message: "password must be more than 8 characters" }, maxLength: { value: 14, message: "password cannot exceed more than 14 characters" } })} />
+                            <p className='alerts'>{errors.password?.message}</p>
+
                         </div>
-                        <div className='password mb-4'>
+                        <div className='password mb-1'>
                             <label className='py-2'>Confirm Password</label>
                             <div className='password-icon'>
                                 <LockIcon />
                             </div>
-                            <div className='eye-icon'>
+                            {
+                                toggle2 ? <div className='eye-icon' onClick={() => { setToggle(!toggle) }}>
+                                <VisibilityIcon />
+                            </div> : <div className='eye-icon' onClick={() => { setToggle(!toggle) }}>
                                 <VisibilityOffIcon />
-                            </div>
-                            <input  type="password" placeholder='6+ Characters' required/>
-                     
+                            </div> 
+                            }
+                            <input
+                            name='confirm_password'
+                            type={toggle2 ? "text" : "password"} placeholder='7+ Characters'
+                                {...register("confirm_password", {
+                                    required: true,
+                                    validate: (val) => {
+                                        if (watch('password') != val) {
+                                            return "Your passwords do no match";
+                                        }
+                                    },
+                                })}
+                            />
+                             <p className='alerts'>{errors.confirm_password?.message}</p>
                         </div>
                         <div className='login-btn mb-3 py-2'>
                             <button className='login-button' type='submit'>Sign in</button>
                         </div>
                     </form>
                     <div className='forgot-password-text'>
-                        <p>Forgot password?</p>
+                    <Link to={"/forgot"} className='Linker' ><p>Forgot password?</p></Link>
                     </div>
                 </div>
             </div>
