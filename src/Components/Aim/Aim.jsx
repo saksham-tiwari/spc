6fd63b42952bd7a79387bc7cecaa57ca679e20b0
saksham-tiwari <sakshamt234@gmail.com>
2023-01-14 00:27:11 +0530

    Removed canopy image

--- src/Components/Aim/Aim.jsx
+++ src/Components/Aim/Aim.jsx
@@ -6,7 +6,7 @@ const Aim = () => {
     <div className={styles.Aim}>
         <h1>Providing employment <span className='empText'>is the aim of the centre.</span></h1>
         <div className={`${styles.card} d-flex align-items-center justify-content-between`}>
-            <img src={aim} alt="aim"/>
+            {/* <img src={aim} alt="aim"/> */}
             <p className='empText'>
             Sustainable Production Center (SPC) is a startup initiated by Value Education (VE) Cell, AKGEC (Ajay Kumar Garg Engineering College). The center is situated at AKGEC campus, the backside of the boy's hostel. The total area of the center is 27 ft x 18 ft approximately. The center was conceptualized in Jan 2022 and finally registered with partnership deeds on 1 July 2022 with a holistic approach. <br/>
 To start with, the Center is preparing handmade soaps using Alovera and Neem grown in the campus. The team collect fresh Alovera and Neem and use them as a soap ingredient without processing them chemically. <br/> <br/>
